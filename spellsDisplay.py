26f15d2befa287d1f5c64a2a06094ccd43c79a23
psyzn1 <psyzn1@nottingham.ac.uk>
2025-10-25 17:02:49 +0100

    New file for spell functions to show images of spells

--- spellsDisplay.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum, auto
-
-
-class Spell(Enum):
-    FIREBALL = auto() # Open palm fingers apart like casting a fireball
-    SHIELD = auto() # Open palm like a stop sign
-    BIND = auto()  #
